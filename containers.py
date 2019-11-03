d640344d100519532840bc328ab678e4dadcccc0
SergeyMorev <morev.s.a@yandex.com>
2019-11-03 21:50:29 +0300

    methods of containers

---
+++ containers.py
@@ -0,0 +1,32 @@
+
+
+List = [1, 2, 3]
+List.append(1)
+List.sort()
+List.clear()
+List.remove(1)
+List.insert(1, 2)
+
+Dict = {1: '1', 2: '2'}
+Dict.keys()
+Dict.get(1)
+Dict.values()
+Dict.update()
+Dict.items()
+
+Set = set(1, 2, 3, 4)
+Set.union({other})
+Set.difference(other)
+Set.issubset(other)
+Set.discard(e)
+Set.intersection(other)
+
+Str = '1234567'
+Str.split()
+Str.upper()
+Str.isnumeric()
+Str.join()
+Str.find()
+
+
+
